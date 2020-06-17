6679f28a3d8974e18a0075aeed59f11cc3bd328c
Thirumal Venkat <thirumal@acalvio.com>
2020-06-17 15:33:32 +0530

    Use Pipenv instead of plain old pip

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 TEMP_DIR="${PWD}/tmp"
 INSTALLER_FILE="gen-id-token.pyz"
 TEMP_INSTALLER_FILE="${TEMP_DIR}/${INSTALLER_FILE}"
@@ -9,10 +9,11 @@ function cleanup {
 }
 trap cleanup EXIT
 # Install all packages for Python2, works with Python3 as well.
-PYTHONWARNINGS=ignore:DEPRECATION pip2 install --no-compile --no-cache --quiet --ignore-installed --requirement requirements.txt --target "${TEMP_DIR}/"
+pipenv lock -r > "${TEMP_DIR}/requirements.txt"
+PYTHONWARNINGS=ignore:DEPRECATION pip2 install --no-compile --no-cache --ignore-installed --requirement "${TEMP_DIR}/requirements.txt" --target "${TEMP_DIR}/"
 touch "${TEMP_DIR}/google/__init__.py"
 find "${TEMP_DIR}" -name '*.dist-info' | xargs rm -r
 cp __main__.py "${TEMP_DIR}/"
-(cd "${TEMP_DIR}" && zip -x "bin/*" -q -r -9 "${TEMP_INSTALLER_FILE}" *)
+(cd "${TEMP_DIR}" && zip -x "bin/*" -x "requirements.txt" -r -9 "${TEMP_INSTALLER_FILE}" *)
 echo "#!/usr/bin/env python" | cat - "${TEMP_INSTALLER_FILE}" > "${INSTALLER_FILE}"
 chmod +x "${INSTALLER_FILE}"
